1a2f2fe4f84ef1538d09d5606b10b1de12e05839
scarfacedeb <scarfacedeb@gmail.com>
2019-10-31 14:38:27 +0500

    Remove leftover Page model

--- app/models/page.rb
+++
@@ -1,8 +0,0 @@
-module NS
-  class Page < ApplicationRecord
-    self.table_name = "pages"
-
-    translates :title, :content
-    accepts_nested_attributes_for :translations, allow_destroy: true
-  end
-end
